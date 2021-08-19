40298097e1ca902e6c0aeec86a205ccc43cc8eb3
Cristi Cretu <cristicretu38@gmail.com>
2021-08-19 09:23:47 +0300

    Adds register form - change from tailwind to chakra

--- lireddit-web/pages/index.tsx
+++
@@ -1,9 +0,0 @@
-import Head from "next/head";
-
-export default function Home() {
-  return (
-    <div className="text-9xl text-blue-600 font-bold">
-      hello nextjs + tailwindcss + typescript
-    </div>
-  );
-}
